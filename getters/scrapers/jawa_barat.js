c8b7a50f0e30e6a7893bd11892aed0aa0f6e772e
hendpraz <mhendryp99@gmail.com>
2020-04-13 20:46:24 +0700

    Add dummy HTML and cheerio parsing

--- getters/scrapers/jawa_barat.js
+++ getters/scrapers/jawa_barat.js
@@ -1,40 +1,48 @@
-const rp = require('request-promise')
+// const rp = require('request-promise')
+let axios = require('axios')
 const cheerio = require('cheerio')
+// const { getChrome } = require('../../libs/chrome-script')
+
+import { getDummyHTML } from './dummy_html'
 
 const url = 'https://pikobar.jabarprov.go.id'
 
 const getHTML = () => 
     new Promise(async (resolve, reject) => {
-        rp(url)
-            .then(function(html){
-                resolve(html)
-            })
-            .catch(function(err){
-                reject(err)
-            });
+        axios.get(url)
+            .then((response) => {
+                if(response.status === 200) {
+                    resolve(response.data) 
+                }
+            }, (error) => reject(error))
     })
 
 export default async function () {
-    const content = await getHTML();
+    const content = await getDummyHTML()
     const $ = cheerio.load(content)
-    const test = $.html()
+
+    const positif = $('div section div div:nth-child(2) div b:nth-child(2)').html();
+    const sembuh = $('div section div:nth-child(2) div:nth-child(2) div b:nth-child(2)').html();
+    const meninggal = $('div section div:nth-child(3) div:nth-child(2) div b:nth-child(2)').html();
+
+    let odp = {};
+    let pdp = {};
+
+    odp.proses = $('div section:nth-child(2) div div:nth-child(2) div div:nth-child(1) span').html();
+    odp.selesai = $('div section:nth-child(2) div div:nth-child(2) div div:nth-child(2) span').html();
+    odp.total = $('div section:nth-child(2) div div:nth-child(2) div div:nth-child(3) span').html();
+
+    pdp.proses = $('div section:nth-child(2) div:nth-child(2) div:nth-child(2) div div:nth-child(1) span').html();
+    pdp.selesai = $('div section:nth-child(2) div:nth-child(2) div:nth-child(2) div div:nth-child(2) span').html();
+    pdp.total = $('div section:nth-child(2) div:nth-child(2) div:nth-child(2) div div:nth-child(3) span').html();
 
     return ({
-        "test": test
-        // "positif":,
-        // "sembuh":,
-        // "meninggal":,
-        // ODP adalah Orang Dalam Pengawasan yang masih dalam proses pemantauan, kalau di data webnya cuma satu angka
-        // nanti jadi satu dulu aja
-        // "odp": {
-        //     "total":,
-        //     "proses":,
-        //     "selesai":,
-        // },
-        // "pdp": {
-        //     "total":,
-        //     "proses":,
-        //     "selesai":,
-        // },
+        positif,
+        sembuh,
+        meninggal,
+        odp,
+        pdp,
+        source: url,
+        lastUpdated: "-"
     })
 }
